2ffea4bd8c3ce9b7000cc1bbee4549dd78cff1d8
markwiz <mmark.kuusik@gmail.com>
2026-01-21 19:47:25 -0800

    vin unique migration

---
+++ Car.Data/Migrations/20260122034654_VinUnique.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Car.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class VinUnique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Vin",
+                table: "Cars",
+                type: "nvarchar(17)",
+                maxLength: 17,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Model",
+                table: "Cars",
+                type: "nvarchar(80)",
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Make",
+                table: "Cars",
+                type: "nvarchar(80)",
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_Vin",
+                table: "Cars",
+                column: "Vin",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Cars_Vin",
+                table: "Cars");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Vin",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(17)",
+                oldMaxLength: 17);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Model",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(80)",
+                oldMaxLength: 80);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Make",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(80)",
+                oldMaxLength: 80);
+        }
+    }
+}
