30bb5b7d8acda9301f8ae7d58afef8abbcc43642
markwiz <mmark.kuusik@gmail.com>
2026-01-21 20:35:31 -0800

    Test2 - Updating values

--- Car.CarTest/CarServiceTests.cs
+++ Car.CarTest/CarServiceTests.cs
@@ -1,12 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Car.ApplicationServices.Services;
+using Car.Core.Dto;
+using Car.Core.Exceptions;
+using Xunit;
 
-namespace Car.CarTest
+namespace Car.CarTest;
+
+public sealed class CarServiceTests
 {
-    internal class CarServiceTests
+    [Fact]
+    public async Task AddAsync_CreatesCar_WithTimestamps()
     {
+        var dbName = Guid.NewGuid().ToString();
+        await using var context = TestDbFactory.CreateContext(dbName);
+        var service = new CarService(context);
+
+        var before = DateTimeOffset.UtcNow;
+
+        var dto = new CarCreateUpdateDto
+        {
+            Make = "Toyota",
+            Model = "Corolla",
+            Year = 2020,
+            Vin = "JT123456789012345",
+            MileageKm = 120000
+        };
+
+        var created = await service.AddAsync(dto);
+
+        var after = DateTimeOffset.UtcNow;
+
+        Assert.NotEqual(Guid.Empty, created.Id);
+        Assert.Equal("Toyota", created.Make);
+        Assert.Equal("Corolla", created.Model);
+        Assert.Equal(2020, created.Year);
+        Assert.Equal("JT123456789012345", created.Vin);
+        Assert.Equal(120000, created.MileageKm);
+
+        Assert.True(created.CreatedAt >= before && created.CreatedAt <= after);
+        Assert.True(created.ModifiedAt >= created.CreatedAt);
     }
-}
+    [Fact]
+    public async Task UpdateAsync_ChangesModifiedAt_AndUpdatesFields()
+    {
+        var dbName = Guid.NewGuid().ToString();
+        await using var context = TestDbFactory.CreateContext(dbName);
+        var service = new CarService(context);
+
+        var created = await service.AddAsync(new CarCreateUpdateDto
+        {
+            Make = "Honda",
+            Model = "Civic",
+            Year = 2019,
+            Vin = "HND12345678901234",
+            MileageKm = 50000
+        });
+
+        var beforeUpdate = await service.GetAsync(created.Id);
+        Assert.NotNull(beforeUpdate);
+
+        await Task.Delay(10);
+
+        var updated = await service.UpdateAsync(created.Id, new CarCreateUpdateDto
+        {
+            Make = "Honda",
+            Model = "Civic Sport",
+            Year = 2019,
+            Vin = "HND12345678901234",
+            MileageKm = 52000
+        });
+
+        Assert.Equal(created.Id, updated.Id);
+        Assert.Equal("Civic Sport", updated.Model);
+        Assert.Equal(52000, updated.MileageKm);
+        Assert.True(updated.ModifiedAt > beforeUpdate!.ModifiedAt);
+    }
+
