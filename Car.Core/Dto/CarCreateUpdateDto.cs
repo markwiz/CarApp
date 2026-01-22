78091b1b62d0f4cf1780ee909d6dae7b3d682b08
markwiz <mmark.kuusik@gmail.com>
2026-01-21 19:29:54 -0800

    Updating CreateUpdate by forcing data Annottations

--- Car.Core/Dto/CarCreateUpdateDto.cs
+++ Car.Core/Dto/CarCreateUpdateDto.cs
@@ -1,13 +1,24 @@
-﻿namespace Car.Core.Dto;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Car.Core.Dto;
 
 public sealed class CarCreateUpdateDto
 {
+    [Required]
+    [StringLength(80)]
     public string Make { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(80)]
     public string Model { get; set; } = string.Empty;
 
+    [Range(1886, 3000)]
     public int Year { get; set; }
 
+    [Required]
+    [StringLength(17, MinimumLength = 11)]
     public string Vin { get; set; } = string.Empty;
 
+    [Range(0, int.MaxValue)]
     public int MileageKm { get; set; }
 }
