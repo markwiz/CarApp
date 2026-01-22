322c07138cb5b3a05f0c6eebb98bf4c67b2e81b1
markwiz <mmark.kuusik@gmail.com>
2026-01-21 20:13:46 -0800

    migration plus testing enviorment

--- Car.Data/Migrations/20260122034654_VinUnique.Designer.cs
+++ Car.Data/Migrations/20260122034654_VinUnique.Designer.cs
@@ -4,6 +4,7 @@ using Car.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Car.Data.Migrations
 {
     [DbContext(typeof(CarContext))]
-    partial class CarContextModelSnapshot : ModelSnapshot
+    [Migration("20260122034654_VinUnique")]
+    partial class VinUnique
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
