f0e394468487c8f7c879ee997d3eb6af10c168c3
markwiz <mmark.kuusik@gmail.com>
2026-01-21 19:38:28 -0800

    Updating Carcontext to check that all columns are filled

--- Car.Data/CarContext.cs
+++ Car.Data/CarContext.cs
@@ -10,4 +10,22 @@ public sealed class CarContext : DbContext
     }
 
     public DbSet<Car.Core.Domain.Car> Cars { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+
+    {
+        modelBuilder.Entity<Car.Core.Domain.Car>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+
+            entity.Property(x => x.Make).IsRequired().HasMaxLength(80);
+            entity.Property(x => x.Model).IsRequired().HasMaxLength(80);
+            entity.Property(x => x.Vin).IsRequired().HasMaxLength(17);
+
+            entity.HasIndex(x => x.Vin).IsUnique();
+
+            entity.Property(x => x.CreatedAt).IsRequired();
+            entity.Property(x => x.ModifiedAt).IsRequired();
+        });
+    }
 }
